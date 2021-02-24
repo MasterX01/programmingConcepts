0320774458cc449d7ceaa1993ab0b0891dabcd21
MasterX01 <aakashsaxena14@gmail.com>
2021-02-24 18:44:16 +0530

    Added files for 2 questions

---
+++ day5/diceNum.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+shuf -i 1-6 -n 1
+
+
+
---
+++ day5/random.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+#Q1. Use Random Function to get a single digit.
+
+randomVariable=$((RANDOM));
+echo $randomVariable
+
+
+
